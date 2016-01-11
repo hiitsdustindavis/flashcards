fac501ee334f6af168a7594f38c256101fbb9662
Dustin Davis and Mollie Bootsma <hiitsdustindavis@gmail.com>
2016-01-11 14:11:54 -0800

    Added toggle functionality to cards/made them discrete

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#flash1").click(function(){
+    $("#question1").toggle();
+    $("#answer1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash2").click(function(){
+    $("#question2").toggle();
+    $("#answer2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash3").click(function(){
+    $("#question3").toggle();
+    $("#answer3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash4").click(function(){
+    $("#question4").toggle();
+    $("#answer4").toggle();
+  });
+});
