20562f52fa217b0e77d47b9cc298735131b9aac9
Dustin Davis and Mollie Bootsma <student@epicodus.com>
2016-01-11 16:03:24 -0800

    experimented with flip, failed, went back to original.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#flash1").click(function(){
+  $("#flash1").click(function() {
     $("#question1").toggle();
     $("#answer1").toggle();
   });
@@ -23,5 +23,103 @@ $(document).ready(function() {
   $("#flash4").click(function(){
     $("#question4").toggle();
     $("#answer4").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash5").click(function(){
+    $("#question5").toggle();
+    $("#answer5").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash6").click(function(){
+    $("#question6").toggle();
+    $("#answer6").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash7").click(function(){
+    $("#question7").toggle();
+    $("#answer7").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash8").click(function(){
+    $("#question8").toggle();
+    $("#answer8").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash9").click(function(){
+    $("#question9").toggle();
+    $("#answer9").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash10").click(function(){
+    $("#question10").toggle();
+    $("#answer10").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash11").click(function(){
+    $("#question11").toggle();
+    $("#answer11").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash12").click(function(){
+    $("#question12").toggle();
+    $("#answer12").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash13").click(function(){
+    $("#question13").toggle();
+    $("#answer13").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash14").click(function(){
+    $("#question14").toggle();
+    $("#answer14").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash15").click(function(){
+    $("#question15").toggle();
+    $("#answer15").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash16").click(function(){
+    $("#question16").toggle();
+    $("#answer16").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash17").click(function(){
+    $("#question17").toggle();
+    $("#answer17").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#flash18").click(function(){
+    $("#question18").toggle();
+    $("#answer18").toggle();
   });
 });
